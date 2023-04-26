03c2db77ae40711697c8375394840e64282becbc
yangnguyenthuy <126770360+yangnguyenthuy@users.noreply.github.com>
2023-04-26 16:02:14 +0700

    Update products.dart

--- lib/model/products.dart
+++ lib/model/products.dart
@@ -1,4 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:untitled/api_connection/api_connection.dart';
+
+Future<List<Products>> fetchProducts() async {
+  final response = await http
+      .post(Uri.parse(API.getProduct));
+
+  // Use the compute function to run parsePhotos in a separate isolate.
+  return parseProducts(response.body);
+}
+
+// A function that converts a response body into a List<Photo>.
+List<Products> parseProducts(String responseBody) {
+  List<Products> products = <Products>[];
+
+  var parsed = jsonDecode(responseBody) as List;
+  parsed.forEach((element) { 
+    products.add(Products.fromJson(element));
+  });
+
+  return products;
+  // //final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  // return parsed.map<Products>((json) => Products.fromJson(json)).toList();
+}
 
 class Products{
   int id;
@@ -8,6 +36,17 @@ class Products{
   double price;
 
   Products({required this.id, required this.title, required this.description, required this.image, required this.price});
+
+  factory Products.fromJson(Map<String, dynamic> json) {
+      var x = Products(
+        id: int.parse(json['id']),
+        title: json['title'],
+        description: json['description'],
+        image: json['image'],
+        price: double.parse(json['price']),
+      );
+      return x;
+  }
 
   static List<Products> init() {
     List<Products> data = [
@@ -77,3 +116,21 @@ class Products{
     return data;
   }
 }
+
+// class ProductsList {
+//   final List<Products> products;
+
+//   ProductsList({
+//     required this.products,
+//   });
+
+//   factory ProductsList.fromJson(List<dynamic> parsedJson) {
+
+//     List<Products> photos = new List.empty();
+//     photos = parsedJson.map((i)=>Photo.fromJson(i)).toList();
+//     return new ProductsList(
+//        products: photos,
+       
+//     );
+//   }
+// }
