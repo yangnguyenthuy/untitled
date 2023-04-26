36be5754ac450c2f032f8407f57ec845528ead2e
yangnguyenthuy <126770360+yangnguyenthuy@users.noreply.github.com>
2023-04-26 16:00:26 +0700

    Update api_connection.dart

--- lib/api_connection/api_connection.dart
+++ lib/api_connection/api_connection.dart
@@ -1,8 +1,8 @@
 class API
 {
-  static const hostConnect = "http://192.168.75.1:81/food_app_api/";
+  static const hostConnect = "http://10.20.21.73:80/food_app_api";
   static const signUp = "$hostConnect/user/signup.php";
   static const signIn = "$hostConnect/user/signin.php";
   static const getProduct = "$hostConnect/product/getproduct.php";
-
+  static const searchProduct = "$hostConnect/product/searchproduct.php";
 }
